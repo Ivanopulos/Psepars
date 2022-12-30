b72d3360bbf4c71688078259fe7b46256604fb25
Ivanopulos <Pochta.rus.lc@gmail.com>
2022-12-30 14:37:39 +0300

    по номерам2

--- pypng.py
+++ pypng.py
@@ -2,14 +2,47 @@ import cv2
 import time
 import numpy as np
 import pyscreenshot as ImageGrab
-import pyautogui
+import pyautogui #pip install opencv-python для pyautogui.locateOnScreen('link.png', region=(x, y-30, 600, 55), confidence=0.95)"
 import time
 from array import *
 
 #variable_name = array(pyscreeze.Box, [10])
-ts = 0.8
-for i in range(20):
-    for u in range(1, 14):
+ts = 1
+ppaus = 1
+# for i in range(40): # резолюция согласовано
+#     for u in range(1, 13):
+#         print(i)
+#         print("-")
+#         print(u)
+#         t = 0
+#         while t == 0:
+#             pprint = pyautogui.locateOnScreen(str(u)+'.png')
+#             time.sleep(ts)
+#             if not pprint is None:
+#                 t=1
+#         if pprint.top > 600 and u == 1:  # down
+#             pyautogui.scroll(-100)
+#             time.sleep(ts/2)
+#             pprint = pyautogui.locateOnScreen(str(u) + '.png')
+#             time.sleep(ts / 2)
+#         pyautogui.moveTo(pprint)
+#         t=0
+#         if u==7:
+#             while t == 0:
+#                 pprint1 = pyautogui.locateOnScreen(str(u) + '.png')
+#                 time.sleep(ts)
+#                 if pprint1 is None:
+#                     t = 1
+#         pyautogui.click()
+#         #if u == 6:
+#         #    pyautogui.write('.')
+#         time.sleep(ts)
+#     time.sleep(ppaus)
+
+
+for i in range(20): #переформ по шаблону очень плохо работает с прокруткой pip install opencv-python
+
+    for u in range(1, 9):
         print(i)
         print("-")
         print(u)
@@ -19,16 +52,60 @@ for i in range(20):
             time.sleep(ts)
             if not pprint is None:
                 t=1
-        if pprint.top > 600 and u == 1:  # down
-            pyautogui.scroll(-170)
+        if pprint.top > 800 and u == 1:  # down
+            pyautogui.scroll(-150)
             time.sleep(ts/2)
             pprint = pyautogui.locateOnScreen(str(u) + '.png')
             time.sleep(ts / 2)
         pyautogui.moveTo(pprint)
-        pyautogui.click()
-        if u == 6:
-            pyautogui.write('.')
+        if u == 1: #проверка на дозагруз страницы
+            t=0
+            while t==0:
+                pyautogui.click()
+                time.sleep(ts / 2)
+                print(1)
+                pprint2 = pyautogui.locateOnScreen(str(u) + '.png')
+                if not pprint2 == pprint and pprint2 is not None:  # при условии что работал клик координаты должны отличаться
+
+                    t = 1
+                    print(pprint)
+                    print(pprint2)
+        else:
+            pyautogui.click()
+
+        #if u == 6:
+        #    pyautogui.write('.')
         time.sleep(ts)
+    time.sleep(ppaus)
+
+
+
+
+
+
+
+
+# for i in range(20):
+#     for u in range(1, 14):
+#         print(i)
+#         print("-")
+#         print(u)
+#         t = 0
+#         while t == 0:
+#             pprint = pyautogui.locateOnScreen(str(u)+'.png')
+#             time.sleep(ts)
+#             if not pprint is None:
+#                 t=1
+#         if pprint.top > 600 and u == 1:  # down
+#             pyautogui.scroll(-170)
+#             time.sleep(ts/2)
+#             pprint = pyautogui.locateOnScreen(str(u) + '.png')
+#             time.sleep(ts / 2)
+#         pyautogui.moveTo(pprint)
+#         pyautogui.click()
+#         if u == 6:
+#             pyautogui.write('.')
+#         time.sleep(ts)
 
 
 
