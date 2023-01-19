90b4dbbceb7dc8a261a7b8d90a470db292399a84
Ivanopulos <Pochta.rus.lc@gmail.com>
2023-01-19 12:24:23 +0300

    по номерам3

--- pypng.py
+++ pypng.py
@@ -7,9 +7,10 @@ import time
 from array import *
 
 #variable_name = array(pyscreeze.Box, [10])
-ts = 1
+ts = 0.4
 ppaus = 1
-# for i in range(40): # резолюция согласовано
+# резолюция согласовано
+# for i in range(40):
 #     for u in range(1, 13):
 #         print(i)
 #         print("-")
@@ -40,8 +41,7 @@ ppaus = 1
 #     time.sleep(ppaus)
 
 
-for i in range(20): #переформ по шаблону очень плохо работает с прокруткой pip install opencv-python
-
+for i in range(40): #переформ по шаблону очень плохо работает с прокруткой pip install opencv-python
     for u in range(1, 9):
         print(i)
         print("-")
@@ -77,95 +77,3 @@ for i in range(20): #переформ по шаблону очень плохо 
         #    pyautogui.write('.')
         time.sleep(ts)
     time.sleep(ppaus)
-
-
-
-
-
-
-
-
-# for i in range(20):
-#     for u in range(1, 14):
-#         print(i)
-#         print("-")
-#         print(u)
-#         t = 0
-#         while t == 0:
-#             pprint = pyautogui.locateOnScreen(str(u)+'.png')
-#             time.sleep(ts)
-#             if not pprint is None:
-#                 t=1
-#         if pprint.top > 600 and u == 1:  # down
-#             pyautogui.scroll(-170)
-#             time.sleep(ts/2)
-#             pprint = pyautogui.locateOnScreen(str(u) + '.png')
-#             time.sleep(ts / 2)
-#         pyautogui.moveTo(pprint)
-#         pyautogui.click()
-#         if u == 6:
-#             pyautogui.write('.')
-#         time.sleep(ts)
-
-
-
-# for i in range(210):
-#     print(i)
-#     pprint = pyautogui.locateOnScreen('print0.png')  # print
-#     if pprint is None:
-#         time.sleep(2)
-#         pprint = pyautogui.locateOnScreen('print0.png')
-#     pyautogui.moveTo(pprint)
-#     pyautogui.click()
-#     time.sleep(0.3)
-#     pprint1 = pyautogui.locateOnScreen('print11.png')  # print next
-#     if pprint1 is None:
-#         time.sleep(1)
-#         pprint1 = pyautogui.locateOnScreen('print11.png')
-#         qq = pprint1.top
-#     pyautogui.moveTo(pprint1)
-#     pyautogui.click()
-#     time.sleep(0.3)
-#     pprint21 = pyautogui.locateOnScreen('print21.png')  # load
-#     if pprint21 is None:
-#         time.sleep(2)
-#         pprint21 = pyautogui.locateOnScreen('print21.png')
-#         qq = pprint21.top
-#     pyautogui.moveTo(pprint21)#1240, 302)
-#     pyautogui.click()
-#     time.sleep(2)
-#     pprint2 = pyautogui.locateOnScreen('print2.png')  # save
-#     if pprint2 is None:
-#         time.sleep(2)
-#         pprint2 = pyautogui.locateOnScreen('print2.png')
-#         qq = pprint2.top
-#     pyautogui.moveTo(pprint2)
-#     pyautogui.click()
-#     time.sleep(1.5)
-#     pprint3 = pyautogui.locateOnScreen('print31.png')  # save
-#     if pprint3 is None:
-#         time.sleep(2)
-#         pprint3 = pyautogui.locateOnScreen('print31.png')
-#         qq = pprint3.top
-#     pyautogui.moveTo(pprint3)
-#     pyautogui.click()
-#     time.sleep(1)
-#     pprint4 = pyautogui.locateOnScreen('print4.png')  # close
-#     if pprint4 is None:
-#         time.sleep(1)
-#         pprint4 = pyautogui.locateOnScreen('print4.png')
-#     pyautogui.moveTo(pprint4)
-#     pyautogui.click()
-#     time.sleep(0.2)
-#     pprint4 = pyautogui.locateOnScreen('print4.png')  # close
-#     pyautogui.moveTo(pprint4)
-#     pyautogui.click()
-#     time.sleep(1)
-#     pprint5 = pyautogui.locateOnScreen('ordown1.png')  # для соглашений
-#     if pprint5 is None:
-#         pprint5 = pyautogui.locateOnScreen('ordown2.png')
-#     pyautogui.moveTo(pprint5)
-#     pyautogui.click()
-#     print(pprint5)
-#     if pprint5.top > 600:  # down
-#         pyautogui.scroll(-170)
